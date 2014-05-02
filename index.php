da1935488360a215fc108029f22c41824d68dc12
Florian Girardey <fgirardey@groupeforum.net>
2014-05-02 17:01:14 +0200

    Add image support

--- index.php
+++ index.php
@@ -1,20 +1,29 @@
 <!DOCTYPE html>
-<html>
+<!--[if lt IE 7]>     <html class="no-js ie6"> <![endif]-->
+<!--[if IE 7]>        <html class="no-js ie7"> <![endif]-->
+<!--[if IE 8]>        <html class="no-js ie8"> <![endif]-->
+<!--[if IE 9]>        <html class="no-js ie9"> <![endif]-->
+<!--[if gt IE 9]><!--><html class="no-js"> <!--<![endif]-->
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>SUCSS - Super Useful CSS</title>
-		<link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
-		<link rel="stylesheet" href="css/style.css">
+		<link type="text/css" rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800">
+		<link type="text/css" rel="stylesheet" href="css/style.css">
+		<script type="text/javascript" src="http://modernizr.com/downloads/modernizr-latest.js"></script>
+		<script type="text/javascript" src="https://raw.githubusercontent.com/scottjehl/Respond/master/dest/respond.min.js"></script>
 	</head>
 	<body>
 		<header class="header row">
-			<div class="col-12">
+			<div class="col-2">
 				<div class="inner">
-					col-12
+					<img width="450" height="200" src="http://lorempicsum.com/futurama/450/200/2" alt="">
 				</div>
 			</div>
+			<div class="col-10">
+				<p>yo</p>
+			</div>
 		</header>
-		<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+		<script type="text/javascript">document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
 	</body>
 </html>
