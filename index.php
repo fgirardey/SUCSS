7d229f77cd6245515e9c8aa175ea2f1a02a0c16d
Florian Girardey <fgirardey@groupeforum.net>
2014-05-02 08:18:02 +0200

    First Commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>SUCSS - Super Useful CSS</title>
+		<link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+		<header class="header row">
+			<div class="col-12">
+				<div class="inner">
+					col-12
+				</div>
+			</div>
+		</header>
+		<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+	</body>
+</html>
