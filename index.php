631020d59ac364021c03555937f17c15f13ee7b3
Florian Girardey <fgirardey@groupeforum.net>
2014-05-04 13:33:55 +0200

    Grid and Type improvements

--- index.php
+++ index.php
@@ -11,119 +11,134 @@
 		<link type="text/css" rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800">
 		<link type="text/css" rel="stylesheet" href="css/style.css">
 		<script type="text/javascript" src="http://modernizr.com/downloads/modernizr-latest.js"></script>
-		<script type="text/javascript" src="https://raw.githubusercontent.com/scottjehl/Respond/master/dest/respond.min.js"></script>
 	</head>
 	<body>
 		<div class="row">
-			<div class="row">
-				<h1>12 Colonnes</h1>
+			<div class="col-12">
+				<div class="row">
+					<div class="col-12">
+						<h1>12 Colonnes</h1>
+					</div>
+				</div>
+				<div class="row wrapper">
+					<!-- (div.col-1>.inner>{col-1})*12 -->
+					<div class="col-1">
+						<div class="inner">col-1</div>
+					</div>
+					<div class="col-1">
+						<div class="inner">col-1</div>
+					</div>
+					<div class="col-1">
+						<div class="inner">col-1</div>
+					</div>
+					<div class="col-1">
+						<div class="inner">col-1</div>
+					</div>
+					<div class="col-1">
+						<div class="inner">col-1</div>
+					</div>
+					<div class="col-1">
+						<div class="inner">col-1</div>
+					</div>
+					<div class="col-1">
+						<div class="inner">col-1</div>
+					</div>
+					<div class="col-1">
+						<div class="inner">col-1</div>
+					</div>
+					<div class="col-1">
+						<div class="inner">col-1</div>
+					</div>
+					<div class="col-1">
+						<div class="inner">col-1</div>
+					</div>
+					<div class="col-1">
+						<div class="inner">col-1</div>
+					</div>
+					<div class="col-1">
+						<div class="inner">col-1</div>
+					</div>
+				</div>
 			</div>
-			<div class="row wrapper">
-				<!-- (div.col-1>.inner>{col-1})*12 -->
-				<div class="col-1">
-					<div class="inner">col-1</div>
+		</div>
+		<div class="row">
+			<div class="col-12">
+				<div class="row">
+					<div class="col-12">
+						<h1>6 Colonnes</h1>
+					</div>
 				</div>
-				<div class="col-1">
-					<div class="inner">col-1</div>
+				<div class="row wrapper">
+					<!-- (div.col-2>.inner>{col-2})*6 -->
+					<div class="col-2">
+						<div class="inner">col-2</div>
+					</div>
+					<div class="col-2">
+						<div class="inner">col-2</div>
+					</div>
+					<div class="col-2">
+						<div class="inner">col-2</div>
+					</div>
+					<div class="col-2">
+						<div class="inner">col-2</div>
+					</div>
+					<div class="col-2">
+						<div class="inner">col-2</div>
+					</div>
+					<div class="col-2">
+						<div class="inner">col-2</div>
+					</div>
 				</div>
-				<div class="col-1">
-					<div class="inner">col-1</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12">
+				<div class="row">
+					<div class="col-12">
+						<h1>4 Colonnes</h1>
+					</div>
 				</div>
-				<div class="col-1">
-					<div class="inner">col-1</div>
-				</div>
-				<div class="col-1">
-					<div class="inner">col-1</div>
-				</div>
-				<div class="col-1">
-					<div class="inner">col-1</div>
-				</div>
-				<div class="col-1">
-					<div class="inner">col-1</div>
-				</div>
-				<div class="col-1">
-					<div class="inner">col-1</div>
-				</div>
-				<div class="col-1">
-					<div class="inner">col-1</div>
-				</div>
-				<div class="col-1">
-					<div class="inner">col-1</div>
-				</div>
-				<div class="col-1">
-					<div class="inner">col-1</div>
-				</div>
-				<div class="col-1">
-					<div class="inner">col-1</div>
+				<div class="row wrapper">
+					<!-- (div.col-3>.inner>{col-3})*4 -->
+					<div class="col-3">
+						<div class="inner">col-3</div>
+					</div>
+					<div class="col-3">
+						<div class="inner">col-3</div>
+					</div>
+					<div class="col-3">
+						<div class="inner">col-3</div>
+					</div>
+					<div class="col-3">
+						<div class="inner">col-3</div>
+					</div>
 				</div>
 			</div>
 		</div>
 		<div class="row">
 			<div class="row">
-				<h1>6 Colonnes</h1>
-			</div>
-			<div class="row wrapper">
-				<!-- (div.col-2>.inner>{col-2})*6 -->
-				<div class="col-2">
-					<div class="inner">col-2</div>
+				<div class="col-12">
+					<h1>3 Colonnes</h1>
 				</div>
-				<div class="col-2">
-					<div class="inner">col-2</div>
-				</div>
-				<div class="col-2">
-					<div class="inner">col-2</div>
-				</div>
-				<div class="col-2">
-					<div class="inner">col-2</div>
-				</div>
-				<div class="col-2">
-					<div class="inner">col-2</div>
-				</div>
-				<div class="col-2">
-					<div class="inner">col-2</div>
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="row">
-				<h1>4 Colonnes</h1>
-			</div>
-			<div class="row wrapper">
-				<!-- (div.col-3>.inner>{col-3})*4 -->
-				<div class="col-3">
-					<div class="inner">col-3</div>
-				</div>
-				<div class="col-3">
-					<div class="inner">col-3</div>
-				</div>
-				<div class="col-3">
-					<div class="inner">col-3</div>
-				</div>
-				<div class="col-3">
-					<div class="inner">col-3</div>
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="row">
-				<h1>3 Colonnes</h1>
 			</div>
 			<div class="row wrapper">
 				<!-- (div.col-4>.inner>{col-4})*3 -->
-				<div class="col-4">
+				<div class="col-l-4">
 					<div class="inner">col-4</div>
 				</div>
-				<div class="col-4">
+				<div class="col-l-4">
 					<div class="inner">col-4</div>
 				</div>
-				<div class="col-4">
+				<div class="col-l-4">
 					<div class="inner">col-4</div>
 				</div>
 			</div>
 		</div>
 		<div class="row">
 			<div class="row">
-				<h1>2 Colonnes</h1>
+				<div class="col-12">
+					<h1>2 Colonnes</h1>
+				</div>
 			</div>
 			<div class="row wrapper">
 				<!-- (div.col-6>.inner>{col-6})*2 -->
@@ -137,13 +152,47 @@
 		</div>
 		<div class="row">
 			<div class="row">
-				<h1>1 Colonne</h1>
+				<div class="col-12">
+					<h1>1 Colonne</h1>
+				</div>
 			</div>
 			<div class="row wrapper">
 				<!-- (div.col-12>.inner>{col-12})*1 -->
 				<div class="col-12">
 					<div class="inner">col-12</div>
 				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12">
+				<h1><a href="#">H1 - Title 1</a><small>Lorem ipsum dolor.</small></h1>
+				<p>
+					<a href="http://lorempicsum.com/rio/200/300/2"><img class="alignright" width="200" height="300" src="http://lorempicsum.com/rio/200/300/2" alt=""></a>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati, distinctio, quia fugiat odit enim atque maiores dolor quaerat odio minima quas totam ad inventore voluptatum magni! Temporibus, exercitationem, facilis, ipsum natus cum magnam hic adipisci accusamus modi facere magni iste quas sequi delectus alias quos suscipit omnis reiciendis repellendus dolorem vero obcaecati incidunt reprehenderit itaque perferendis maxime excepturi deleniti quam.
+				</p>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, aperiam, hic eius ad voluptates officia praesentium dicta dignissimos eligendi at cumque itaque eaque cupiditate deleniti pariatur veniam quis ea nam doloribus velit quaerat suscipit iure esse laboriosam reiciendis maxime ducimus quia porro neque corporis! Aliquid, atque, nisi, maiores, sapiente iste officiis earum sint dolores reiciendis similique facere rem? Minus, repudiandae!
+
+				</p>
+				<blockquote>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, officia, quasi alias aut libero quae quos labore non ipsum magnam molestias cumque illum beatae ipsa.
+					</p>
+				</blockquote>
+				<blockquote>
+				<h2>H2 - Title 2</h2>
+				<h3>H3 - Title 3</h3>
+				<h4>H4 - Title 4</h4>
+				<h5>H5 - Title 5</h5>
+				<h6>H6 - Title 6</h6>
+			</div>
+			<div class="col-12">
+				<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, numquam.</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12">
+				<h1 class="text-center full-width">Je suis un titre avec le design de SUCSS</h1>
 			</div>
 		</div>
 		<script type="text/javascript">document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
