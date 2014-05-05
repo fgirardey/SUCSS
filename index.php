0cc4fda69f9d7981aa0e8decbe8534d29aab2d3a
Florian Girardey <fgirardey@groupeforum.net>
2014-05-05 17:39:45 +0200

    Type + Global update

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>SUCSS - Super Useful CSS</title>
-		<link type="text/css" rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800">
 		<link type="text/css" rel="stylesheet" href="css/style.css">
 		<script type="text/javascript" src="http://modernizr.com/downloads/modernizr-latest.js"></script>
 	</head>
@@ -16,7 +15,7 @@
 		<div class="row">
 			<div class="col-12">
 				<div class="row">
-					<div class="col-12">
+					<div style="" class="col-12">
 						<h1>12 Colonnes</h1>
 					</div>
 				</div>
@@ -166,35 +165,83 @@
 		<div class="row">
 			<div class="col-12">
 				<h1><a href="#">H1 - Title 1</a><small>Lorem ipsum dolor.</small></h1>
+				<h2>H2 - Title 2</h2>
+				<h3>H3 - Title 3</h3>
+				<h4>H4 - Title 4</h4>
+				<h5>H5 - Title 5</h5>
+				<h6>H6 - Title 6</h6>
 				<p>
 					<a href="http://lorempicsum.com/rio/200/300/2"><img class="alignright" width="200" height="300" src="http://lorempicsum.com/rio/200/300/2" alt=""></a>
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati, distinctio, quia fugiat odit enim atque maiores dolor quaerat odio minima quas totam ad inventore voluptatum magni! Temporibus, exercitationem, facilis, ipsum natus cum magnam hic adipisci accusamus modi facere magni iste quas sequi delectus alias quos suscipit omnis reiciendis repellendus dolorem vero obcaecati incidunt reprehenderit itaque perferendis maxime excepturi deleniti quam.
 				</p>
 				<p>
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, aperiam, hic eius ad voluptates officia praesentium dicta dignissimos eligendi at cumque itaque eaque cupiditate deleniti pariatur veniam quis ea nam doloribus velit quaerat suscipit iure esse laboriosam reiciendis maxime ducimus quia porro neque corporis! Aliquid, atque, nisi, maiores, sapiente iste officiis earum sint dolores reiciendis similique facere rem? Minus, repudiandae!
-
 				</p>
+				<p>
+					<abbr title="Cascading Steelsheet">CSS</abbr>
+				</p>
+				<blockquote>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, officia, quasi alias aut libero quae quos labore non ipsum magnam molestias cumque illum beatae ipsa.
+					</p>
+					<footer><cite>Florian Girardey</cite></footer>
+				</blockquote>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12">
+				<h1 class="text-center full-width">Je suis un titre avec le design de <abbr title="Super Usefull CSS">SUCSS</abbr></h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12">
+				<h1>Blockquotes</h1>
+				<blockquote>
+					Lorem ipsum dolor <cite>sit amet</cite>, consectetur adipisicing elit. Quidem, officia, quasi alias aut libero quae quos labore non ipsum magnam molestias cumque illum beatae ipsa.
+				</blockquote>
 				<blockquote>
 					<p>
 						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, officia, quasi alias aut libero quae quos labore non ipsum magnam molestias cumque illum beatae ipsa.
 					</p>
 				</blockquote>
 				<blockquote>
-				<h2>H2 - Title 2</h2>
-				<h3>H3 - Title 3</h3>
-				<h4>H4 - Title 4</h4>
-				<h5>H5 - Title 5</h5>
-				<h6>H6 - Title 6</h6>
-			</div>
-			<div class="col-12">
-				<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, numquam.</h1>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, officia, quasi alias aut libero quae quos labore non ipsum magnam molestias cumque illum beatae ipsa.
+					</p>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, officia, quasi alias aut libero quae quos labore non ipsum magnam molestias cumque illum beatae ipsa.
+					</p>
+				</blockquote>
+				<blockquote>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, officia, quasi alias aut libero quae quos labore non ipsum magnam molestias cumque illum beatae ipsa.
+					</p>
+					<cite>Florian Girardey</cite>
+				</blockquote>
+				<div class="radius">
+					<blockquote class="blockquote--highlight">
+						<p>
+							J'aime les <mark>kiwis</mark> <ins>mais aussi les cerises</ins> et les bananes !
+						</p>
+						<footer>
+							<cite>Florian Girardey</cite>
+						</footer>
+					</blockquote>
+				</div>
+				<p>
+					This is a <mark>paragraph</mark> typed
+				</p>
+				<address>
+					<strong>Twitter, Inc.</strong><br>
+					795 Folsom Ave, Suite 600<br>
+					San Francisco, CA 94107<br>
+					<abbr title="Phone">P:</abbr> (123) 456-7890
+				</address>
+				<address>
+					<strong>Full Name</strong><br>
+					<a href="mailto:#">first.last@example.com</a>
+				</address>
 			</div>
 		</div>
-		<div class="row">
-			<div class="col-12">
-				<h1 class="text-center full-width">Je suis un titre avec le design de SUCSS</h1>
-			</div>
-		</div>
-		<script type="text/javascript">document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
 	</body>
 </html>
